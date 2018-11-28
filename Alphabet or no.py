e26379434c681f8179669fbde9ed8d0f5a04f45f
Jayapraveen <43662378+Jayapraveen34@users.noreply.github.com>
2018-11-28 19:34:41 +0530

    Add files via upload

---
+++ Alphabet or no.py
@@ -0,0 +1,6 @@
+git = str(input('enter the letter :'))
+hub = ('a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z')
+if git in hub:
+    print('Alphabet')
+else:
+    print('No')
