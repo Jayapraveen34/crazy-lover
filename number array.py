cf403943a708cb5ee752215217269c391218bc1d
Jayapraveen <43662378+Jayapraveen34@users.noreply.github.com>
2018-12-04 21:35:11 +0530

    Add files via upload

---
+++ number array.py
@@ -0,0 +1,12 @@
+a=int(input('enter the inpuut:'))
+if a<=0:
+    print('invalid input')
+else:
+    sum=0
+    for i in range(1,a+1):
+	    sum=sum+i
+    print(sum)
+        
+        
+    
+    
