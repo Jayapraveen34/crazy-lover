de81cdbc53964881a264f6d872efb6f4ffd1dd7f
Jayapraveen <43662378+Jayapraveen34@users.noreply.github.com>
2018-12-04 22:09:52 +0530

    Add files via upload

---
+++ prime finder.py
@@ -0,0 +1,10 @@
+a=int(input('Enter the number'))
+if a== (1 or 2 or 3 or 5 or 7):
+    print('prime number')
+    
+elif (a%2==0 or a%3==0 or a%5==0 or a%7==0):
+    print('not prime number')
+    
+else:
+    print('This is prime number')
+    
