8c6d85632440f63e0a2598df3f96398a10601a3f
Jayapraveen <43662378+JayapraveenS@users.noreply.github.com>
2021-01-28 15:09:32 +0530

    Create DuplicateWordRemover.java

---
+++ DuplicateWordRemover.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        if(input.hasNextLine())
+        {
+        String[] userInput;
+           userInput=input.nextLine().split(" ");
+        String output="";
+        int count=0;
+        for(String c:userInput)
+        {
+            for(String d:userInput)
+            {
+                if(c.equals(d))
+                    count++;
+            }
+            if(count==1)
+                output+=(" "+c);
+            count=0;
+        }
+        System.out.println(output);
+        }
+    }
+}
